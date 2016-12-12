02fa9bd52fc2cec5fe8d87a4dc8246184d8aee37
Akhilanand Singh <akhilanandsingh@gmail.com>
2016-12-12 18:10:13 +0530

    added fabric.js

--- public/upload.js
+++ public/upload.js
@@ -1,14 +1,57 @@
-var col = true;
-	
-function uploadPic() {
-    var Pic = document.getElementById("myCanvas").toDataURL("image/png");
+var colBoolean = false;
+
+function getColor(){
+    var xcol = "";
+    if (colBoolean){
+        xcol = "green";
+        colBoolean = false;
+    }else{
+        xcol = "red";
+        colBoolean = true;
+    }
+    return xcol;
+}	
+
+function init(){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.font = "40px Cambria";
+    ctx.strokeStyle = "green";
+    ctx.strokeText("Got Print", 100, 40);
+}
+init();
+
+
+function rectangle(){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.translate(20, 0);
+    ctx.fillStyle = getColor();
+     ctx.fillRect(20, 50, 19, 19);
+}
+
+function circle(){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");    
+    ctx.translate(-22, 0);
+    ctx.fillStyle = getColor();
+    ctx.beginPath();
+    ctx.arc(20, 100, 10, 0, 2.0*Math.PI);
+    ctx.stroke();
+    ctx.fill();
+}
+
+
+
+function uploadPic(canvas, imgName) {
+    var Pic = document.getElementById(canvas).toDataURL("image/png");
     Pic = Pic.replace(/^data:image\/(png|jpg);base64,/, "")
-    console.log("Uploading to server");
+    console.log("Uploading to server, imgName=" + imgName);
     // Sending the image data to Server
     $.ajax({
         type: 'POST',
         url: '/upload',
-        data: '{ "imageData" : "' + Pic + '" }',
+        data: '{ "imageData" : "' + Pic + '"}',
         contentType: 'application/json; charset=utf-8',
         dataType: 'json',
         success: function (msg) {
@@ -19,32 +62,39 @@ function uploadPic() {
 
 }//uploadPic
 
-function rectangle(){
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-    ctx.font = "40px Arial";
-    ctx.strokeText("Got Print", 100, 40);
-    ctx.fillStyle = "green";
-    ctx.fillRect(50, 50, 15, 15);
+/* *************************************************************************  */
+var x2 = 0;
+var fabCanvas;
+function init2(){
+    fabCanvas = new fabric.Canvas("c");
+    var fabCircle = new fabric.Circle({ 
+        radius: 20, fill: '#f55', top: 10, left: 300 
+    });
 
-    col = false;
+    var txt = new fabric.Text("Hello Fabric.js", {
+        shadow: 'rgba(0,0,0,0.2) 0 0 5px'
+    });
+
+    fabCanvas.add(txt);
+    fabCanvas.add(fabCircle);
 }
+init2();
 
-function move(){
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-
-    ctx.translate(40, 0);
-    if (col){
-        ctx.fillStyle = "green";
-        col = false;
-    }else{
-        ctx.fillStyle = "red";
-        col = true;
-    }
-    
-    ctx.fillRect(50, 50, 15, 15);
+function addTriangle(){
+    x2 += 50;
+    fabCanvas.add(new fabric.Triangle({
+                width: 30, height: 30, fill: getColor(), left: x2, top: 50
+    }));
 }
-
-rectangle();
+function addRect(){
+    x2 += 50;
+    fabCanvas.add(new fabric.Rect({
+        left: x2,
+        top: 100,
+        fill: getColor(),
+        width: 30,
+        height: 30,
+        angle: 45
+    }));
+}
 
--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 exports.uploadHandler = function(req, res){
 	console.log("Upload Handler called");
 	var imgData = req.body.imageData;
-	//console.log(imgData);
+	//console.log("req.body.imageName=" + req.body.imageName);
 	var buf = new Buffer(imgData, 'base64');
 	fs.writeFileSync('./public/image.png', buf);
 	var html = "<html><body><img src='image.png'></img></body></html>";
